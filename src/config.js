ead33cf97031102fbf1a2b6acf8bb4f78042b692
Manh Pham <manhhailua@gmail.com>
2016-08-15 14:49:39 +0700

    change port to 4000

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@
 
 /* eslint-disable max-len */
 
-export const port = process.env.PORT || 3000;
+export const port = process.env.PORT || 4000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
 export const databaseUrl = process.env.DATABASE_URL || 'sqlite:database.sqlite';
