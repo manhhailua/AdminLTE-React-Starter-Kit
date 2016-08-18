5929214265d1a554aac00e7e4a096cce3d73cbb2
Manh Pham <manhhailua@gmail.com>
2016-08-18 10:55:41 +0700

    change route:home

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -17,22 +17,7 @@ function Home({news}, context) {
   context.setTitle(title);
 
   return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1 className={s.title}>React.js News</h1>
-        <ul className={s.news}>
-          {news.map((item, index) => (
-            <li key={index} className={s.newsItem}>
-              <a href={item.link} className={s.newsTitle}>{item.title}</a>
-              <span
-                className={s.newsDesc}
-                dangerouslySetInnerHTML={{__html: item.contentSnippet}}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <p>This is the home page!</p>
   );
 }
 
